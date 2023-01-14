71c4392a769f3a43a4e2eacd288a9a8b8e2d3ed0
sinasb87 <116197226+sinasb87@users.noreply.github.com>
2023-01-14 21:01:28 +0330

    Add files via upload

---
+++ src/inputStreamExample.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class inputStreamExample {
+        public static void main(String args[]){
+            try{
+                //fine = file input stream example
+                FileInputStream fine =new FileInputStream("C:\\Users\\Sina\\Desktop\\readsAllCharacters.txt");
+                int i=0;
+                // -1 means until it has more characters to read
+                while((i=fine.read())!=-1){
+                    System.out.print((char)i);
+                }
+                fine.close();
+            }catch(Exception e){System.out.println(e);}
+        }
+    }
+
---
+++ src/outputStreamExample.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class outputStreamExample {
+        public static void main(String args[]){
+            try{
+                FileOutputStream fose=new FileOutputStream("C:\\Users\\Sina\\Desktop\\readsAllCharacters.txt");
+                String se="hello this is my example";
+                byte b[]=se.getBytes();
+                fose.write(b);
+                fose.close();;
+            }catch(Exception e){System.out.println(e);}
+        }
+    }
