3e00046ded5037b5cc9825e443c4d14ab374909d
sinasb87 <116197226+sinasb87@users.noreply.github.com>
2023-01-14 21:26:37 +0330

    Update outputStreamExample.java

--- src/outputStreamExample.java
+++ src/outputStreamExample.java
@@ -3,8 +3,8 @@ public class OutputStreamExample {
         public static void main(String args[]){
             try{
                 FileOutputStream fose=new FileOutputStream("C:\\Users\\Sina\\Desktop\\readsAllCharacters.txt");
-                String se="hello this is my example";
-                byte b[]=se.getBytes();
+                String se = "hello this is my example";
+                byte b[] = se.getBytes();
                 fose.write(b);
                 fose.close();;
             }catch(Exception e){System.out.println(e);}
